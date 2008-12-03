77eae7d4d1543ecfbea8706d8d86a9a6a50d4f91
Vasily Chekalkin <bacek@bacek.com>
2008-12-03 14:52:47 +1100

    Add dumping of parsed grammar

--- r.pl
+++ r.pl
@@ -4,4 +4,4 @@ use XQuery::Parser::Grammar;
 say @*ARGS[0];
 my $filename = @*ARGS[0];
 my $content = slurp $filename;
-say $content ~~ XQuery::Parser::Grammar::TOP;
+say ($content ~~ XQuery::Parser::Grammar::TOP).perl;
