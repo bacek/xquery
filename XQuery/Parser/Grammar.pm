759a79ff15b002b7ada7b3e01cb1e8d03f845bd7
Vasily Chekalkin <bacek@bacek.com>
2008-12-06 12:14:11 +1100

    Hooray. Comments parsed correctly! thei0s++

--- XQuery/Parser/Grammar.pm
+++ XQuery/Parser/Grammar.pm
@@ -303,7 +303,7 @@ grammar XQueryGrammar {
 #[151]    	Comment 	   ::=    	"(:" (CommentContents | Comment)* ":)" 	/* ws: explicit */
 #				/* gn: comments */
     #token Comment         { '(:' [ <CommentContents> | <Comment> ]*':)' }
-    token Comment         { '(:' <- [ '(:' | ':) ]>* ':)' }
+    token Comment         { '(:' <CommentContents>* ':)' }
     #token Comment         { '(:' <CommentContents>? ':)' }
 #[152]    	PITarget 	   ::=    	[http://www.w3.org/TR/REC-xml#NT-PITarget] XML 	/* xgs: xml-version */
 #[153]    	CharRef 	   ::=    	[http://www.w3.org/TR/REC-xml#NT-CharRef] XML 	/* xgs: xml-version */
@@ -324,7 +324,7 @@ grammar XQueryGrammar {
 #The following symbols are used only in the definition of terminal symbols; they are not terminal symbols in the grammar of A.1 EBNF.
 #[158]    	Digits 	   ::=    	[0-9]+
 #[159]    	CommentContents 	   ::=    	(Char+ - (Char* ('(:' | ':)') Char*))
-    token  CommentContents  {  <- [ '(:' | ':)']>* };
+    token CommentContents { .*? <before '(:' | ':)'> };
 
     token S { \h | \v };
     token ws_all { <.S> | <.Comment> };
