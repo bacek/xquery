4cfa4dd2c2cf38354e544db1388d4c3fa42dc46b
Vasily Chekalkin <bacek@bacek.com>
2008-12-07 00:01:41 +1100

    Implement skipping tests in TestUtil

--- TestUtil.pm
+++ TestUtil.pm
@@ -7,10 +7,13 @@ Helper function for running XQTS based tests.
 =end
 =cut
 
-sub xqts(:$count, :$dirname, :$filemask, :@expect_fail = (), :@todo = ()) {
+sub xqts(:$count, :$dirname, :$filemask, :@expect_fail = (), :@todo = (), :@skip = ()) {
     my %expect_fail = map { $^a => 1 }, @expect_fail;
     my %todo = map { $^a => 1 }, @todo;
+    my %skip = map { $^a => 1 }, @skip;
     for (1..$count) -> $test {
+        next if %skip.exists($test);
+
         my $filename = sprintf($filemask, $test);
         my $content = slurp $dirname ~ $filename;
         my $res = $content ~~ / <XQueryGrammar::TOP> /;
