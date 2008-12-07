34572c4dee468d6da3dba119943480c0e895b9fa
Vasily Chekalkin <bacek@bacek.com>
2008-12-08 07:07:21 +1100

    Remove .perl call for Match

--- r.pl
+++ r.pl
@@ -1,8 +1,12 @@
 use v6;
+use Data::Dumper;
 use XQuery::Parser::Grammar;
 
 say @*ARGS[0];
 my $filename = @*ARGS[0];
 my $content = slurp $filename;
-say ($content ~~ / <XQueryGrammar::TOP>/).perl;
-#say $/.perl;
+say ($content ~~ XQueryGrammar);
+#say '==== match.perl ======';
+#say $d.dumper( $/ );
+#my %h = $/;
+#say $/<Module><MainModule><Prolog><VarDecl>[0]<external>;
