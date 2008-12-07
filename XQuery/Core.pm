51079954ccfd408ed01d8f31b42393fd4a0ec112
Vasily Chekalkin <bacek@bacek.com>
2008-12-07 21:59:13 +1100

    Start implementing converting AST to Core

--- XQuery/Core.pm
+++ XQuery/Core.pm
@@ -17,8 +17,19 @@ sub create_module($module) {
     create_expr($module<MainModule><QueryBody><Expr>);
 };
 
-sub create_expr($expr) {
-    XQuery::Core::Node.new();
+sub create_expr($expr, $pos = 0) {
+    return unless $expr<ExprSingle>[$pos];
+
+    my $expr_single = $expr<ExprSingle>[$pos];
+    
+    # recursivly process ExprSingle.
+    create_expr_single($expr_single, create_expr($expr, ++$pos));
+};
+
+sub create_expr_single($ast, $left) {
+    XQuery::Core::FLWORExpr.new(
+        left => $left
+    );
 };
 
 # vim: ft=perl6
