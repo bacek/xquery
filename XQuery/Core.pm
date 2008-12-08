76321b8787f973eafca98c5a1f8c4752285b5913
Vasily Chekalkin <bacek@bacek.com>
2008-12-09 00:18:51 +1100

    Add create_expr_single

--- XQuery/Core.pm
+++ XQuery/Core.pm
@@ -21,14 +21,48 @@ sub create_expr($expr, $pos = 0) {
     return unless $expr<ExprSingle>[$pos];
 
     my $expr_single = $expr<ExprSingle>[$pos];
-    
+
     # recursivly process ExprSingle.
     create_expr_single($expr_single, create_expr($expr, ++$pos));
 };
 
 sub create_expr_single($ast, $left) {
-    XQuery::Core::FLWORExpr.new(
-        left => $left
+=begin cut
+# it doesn't work in Rakudo.
+    given $ast {
+        when .<FLWORExpr>      { create_flwor_expr($_, $left) }
+        when .<QuantifiedExpr> { create_quantified_expr($_, $left) }
+        when .<TypeswitchExpr> { create_typeswitch_expr($_, $left) }
+        when .<IfExpr>         { create_if_expr($_, $left) }
+        when .<OrExpr>         { create_or_expr($_, $left) }
+    }
+=end cut
+    my &sub;
+    if ($ast<FLWORExpr>) {
+        return create_flwor_expr($ast<FLWORExpr>, $left);
+    }
+    elsif ( $ast<QuantifiedExpr>) {
+        return create_quantified_expr($ast<QuantifiedExpr>, $left);
+    }
+    elsif ( $ast<TypeswitchExpr>) {
+        return create_typeswitch_expr($ast<TypeswitchExpr>, $left);
+    }
+    elsif ( $ast<IfExpr>) {
+        return create_if_expr($ast<IfExpr>, $left);
+    }
+    elsif ( $ast<OrExpr> ) {
+        return create_or_expr($ast<OrExpr>, $left);
+    }
+
+    die('holy shit');
+};
+
+
+
+
+sub create_or_expr($or, $left) {
+    XQuery::Core::Literal.new(
+        value => ~$or
     );
 };
 
