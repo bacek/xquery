ee2100df6a109b574b35b756e8d926973460b829
Vasily Chekalkin <bacek@bacek.com>
2008-12-09 07:50:50 +1100

    Add small description for Core

--- XQuery/Core.pm
+++ XQuery/Core.pm
@@ -3,6 +3,15 @@ use XQuery::Core::Creator;
 
 package XQuery::Core;
 
+=begin pod
+
+Core is tree, almost identical to XQuery Formal Semantics Core tree.
+Each Node has attached StaticType and optional StaticContext.
+StaticContext used during compilation/validation/execution phase.
+StaticType used during optimization phase.
+
+=end pod
+
 sub create($ast) {
     XQuery::Core::Creator.new().create_module($ast<Module>);
 };
